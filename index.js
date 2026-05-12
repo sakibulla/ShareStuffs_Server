610898c7ad0848bf0a691ee6f62a3e56cd4e2aef
Sakib fahmid <hasanfahmidul2002@gmail.com>
2026-05-13 00:03:03 +0600

    SOme FIxing

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ require("dotenv").config();
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 // test route
